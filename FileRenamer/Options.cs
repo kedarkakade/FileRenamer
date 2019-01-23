412dce9f20e693819fb5243e8299af24ece2f829
Kedar Kakade <kedarkakade@gmail.com>
2019-01-23 07:50:54 -0500

    Initial Checkin

---
+++ FileRenamer/Options.cs
@@ -0,0 +1,37 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using System.Collections.Generic;
+
+namespace FileRenamer
+{
+    public class Options
+    {
+        [Option('v', Required = false, HelpText = "Set output to verbose messages.")]
+        public bool Verbose { get; set; }
+
+        [Option('r', Required = false, HelpText = "Scans current folder as well as sub folders recursively.")]
+        public bool Recursive { get; set; }
+
+        [Option('p', Required = true, HelpText = "Set input/source folder path.")]
+        public string Path { get; set; }
+
+        [Option('i', Required = true, HelpText = "Input file extension. e.g. \".js\".")]
+        public string InputExtension { get; set; }
+
+        [Option('o', Required = true, HelpText = "Output file extension. e.g. \".js.txt\".")]
+        public string OutputExtension { get; set; }
+
+        [Usage(ApplicationAlias = "FileRenamer")]
+        public static IEnumerable<Example> Examples
+        {
+            get
+            {
+                return new List<Example>()
+                {
+                    new Example("\nNormal Mode:", new Options { InputExtension=".js", OutputExtension=".js.txt", Path= @"C:\tmp\" } ),
+                    new Example("\nRecursive Mode:", new Options { InputExtension=".js", OutputExtension=".js.txt", Path=@"C:\tmp\", Recursive=true })
+                };
+            }
+        }
+    }
+}
---
+++ FileRenamer/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using CommandLine;
+
+namespace FileRenamer
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(opts => RunOptionsAndReturnExitCode(opts));
+        }
+
+        private static void RunOptionsAndReturnExitCode(Options opts)
+        {
+            if(opts != null)
+            {
+                Console.WriteLine("Verbose: {0}", opts.Verbose);
+                Console.WriteLine("Recursive: {0}", opts.Recursive);
+                Console.WriteLine("Input Extension: {0}", opts.InputExtension);
+                Console.WriteLine("Output Extension: {0}", opts.OutputExtension);
+                Console.WriteLine("Path: {0}", opts.Path);
+
+                CleanupParameters(ref opts);
+                StartProcessingFiles(opts);
+            }
+        }
+
+        private static void StartProcessingFiles(Options opts)
+        {
+            var dirInfo = new DirectoryInfo(opts.Path);
+            var oldExtension = GetReplaceableFileExtension(opts.InputExtension);
+            var files = opts.Recursive? dirInfo.EnumerateFiles(opts.InputExtension, SearchOption.AllDirectories) 
+                : dirInfo.EnumerateFiles(opts.InputExtension, SearchOption.TopDirectoryOnly);
+
+            foreach (var file in files)
+            {
+                var outputFile = file.FullName.Replace(oldExtension, opts.OutputExtension);
+                file.MoveTo(outputFile);
+            }
+        }
+
+        private static void CleanupParameters(ref Options opts)
+        {
+            opts.InputExtension = GetSearchableFileExtension(opts.InputExtension);
+
+            opts.OutputExtension = GetReplaceableFileExtension(opts.OutputExtension);
+        }
+
+        private static string GetSearchableFileExtension(string extension)
+        {
+            var newExtension = extension;
+
+            if (!newExtension.StartsWith("*") && newExtension.StartsWith("."))
+            {
+                newExtension = "*" + newExtension;
+            }
+            else if (!newExtension.StartsWith("*") && !newExtension.StartsWith("."))
+            {
+                newExtension = "*." + newExtension;
+            }
+
+            return newExtension;
+        }
+
+        private static string GetReplaceableFileExtension(string extension)
+        {
+            var newExtension = extension;
+
+            if (newExtension.StartsWith("*") && !newExtension.StartsWith("."))
+            {
+                newExtension = newExtension.Substring(1);
+            }
+            else if (!newExtension.StartsWith("."))
+            {
+                newExtension = "." + newExtension;
+            }
+
+            return newExtension;
+        }
+    }
+}
